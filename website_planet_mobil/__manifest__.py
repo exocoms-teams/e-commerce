25743d4e74474f950e751fb3d88bcb225dff38ec
Laeticia <laeticiabamba@icloud.com>
2026-05-13 23:01:22 +0200

    Ajout du code de module

---
+++ website_planet_mobil/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Website Planet Mobil',
+    'version': '1.0',
+    'category': 'Website',
+    'summary': 'Personnalisation de la page d accueil Planet Mobil',
+    'depends': ['website', 'website_sale'],
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
