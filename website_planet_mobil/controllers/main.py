e0927d22a7cad24c96eee288e3672fb6b28caa7a
Laeticia <laeticiabamba@icloud.com>
2026-05-14 23:09:33 +0200

    correction du code

--- website_planet_mobil/controllers/main.py
+++ website_planet_mobil/controllers/main.py
@@ -1,21 +1,2 @@
+
 from odoo import http
-from odoo.http import request
-
-class PlanetMobilController(http.Controller):
-
-    @http.route('/', type='http', auth='public', website=True)
-    def homepage(self, **kwargs):
-        new_products = request.env['product.template'].sudo().search([
-            ('is_published', '=', True),
-            ('is_new', '=', True),
-        ], limit=4)
-
-        best_sellers = request.env['product.template'].sudo().search([
-            ('is_published', '=', True),
-            ('is_best_seller', '=', True),
-        ], limit=4)
-
-        return request.render('website_planet_mobil.homepage', {
-            'new_products': new_products,
-            'best_sellers': best_sellers,
-        })
