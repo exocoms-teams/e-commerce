34da2afde30750379a68e14b9d5e5894479141d5
Axel23 <kanamarini@gmail.com>
2026-05-14 03:35:02 +0200

    mise a jour du site

--- exocoms_theme/controllers/main.py
+++ exocoms_theme/controllers/main.py
@@ -1,13 +1,18 @@
 from odoo import http
 from odoo.http import request
 
+
 class Exocoms(http.Controller):
 
     @http.route('/', type='http', auth='public', website=True, sitemap=True)
     def home(self, **kw):
+
         return request.render('exocoms_theme.home', {})
+
+    @http.route('/services', type='http', auth='public', website=True, sitemap=True)
     def services_page(self, **kw):
 
         return request.render(
-            'exocoms_theme.services_page'
+            'exocoms_theme.services_page',
+            {}
         )
