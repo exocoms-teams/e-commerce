35e2245a43e54c090ec34066383f9a95462f3c28
Axel23 <kanamarini@gmail.com>
2026-05-25 16:34:31 +0200

    mon travail local

--- exocoms_theme/__manifest__.py
+++ exocoms_theme/__manifest__.py
@@ -6,27 +6,20 @@
     'license': 'LGPL-3',
     'category': 'Website',
     'depends': [
-        'website','website_sale'
+        'website', 'website_sale'
     ],
     'data': [
-        #DATA
         'data/website_data.xml',
-        # PAGES
         'views/pages/home.xml',
         'views/pages/services.xml',
         'views/pages/mentions_legales.xml',
-         'views/pages/boutique.xml',
-
-        # PARTIALS
+        'views/pages/boutique.xml',
         'views/partials/hero.xml',
         'views/partials/dashbord.xml',
         'views/partials/dashbord_boutique.xml',
         'views/partials/services_hero.xml',
         'views/partials/services_content.xml',
         'views/partials/mentions_legales_content.xml',
-        
-
-        # TEMPLATES
         'views/templates/header.xml',
         'views/templates/footer.xml',
         'views/templates/features.xml',
@@ -35,9 +28,7 @@
     ],
     'assets': {
         'web.assets_frontend': [
-            # CSS - DESIGN SYSTEM & CORE
             'exocoms_theme/static/src/css/layout.css',
-            # CSS - COMPONENTS
             'exocoms_theme/static/src/css/header.css',
             'exocoms_theme/static/src/css/hero.css',
             'exocoms_theme/static/src/css/features.css',
@@ -52,16 +43,19 @@
             'exocoms_theme/static/src/css/services_hero.css',
             'exocoms_theme/static/src/css/home.css',
             'exocoms_theme/static/src/css/legal.css',
-            # CSS - ANIMATIONS & UTILITIES
             'exocoms_theme/static/src/css/animations.css',
             'exocoms_theme/static/src/css/benefits.css',
+            # Fichiers manquants ajoutés
+            'exocoms_theme/static/src/css/cards.css',
+            'exocoms_theme/static/src/css/pages.css',
+            'exocoms_theme/static/src/css/responsive.css',
+            'exocoms_theme/static/src/css/sections.css',
+            'exocoms_theme/static/src/css/home_sections.css',
+            'exocoms_theme/static/src/css/odoo-integration.css',
             # JS
             'exocoms_theme/static/src/js/main.js',
-            #'exocoms_theme/static/src/js/shop_redirect.js',
         ],
     },
-
     'installable': True,
-
     'application': True,
 }
