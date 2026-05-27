bd89619bfbb10c18ddada016200aa78c6599e705
Chouikhi Imene <imene.chouikhi@etu.univ-lyon1.fr>
2026-05-27 11:02:35 +0200

    correctif + ajout product_extension

---
+++ website_planet_mobil/models/product_extension.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    x_is_top_vente = fields.Boolean(string='Top Vente', default=False)
+    x_is_nouveaute = fields.Boolean(string='Nouveauté', default=False)
+    x_is_promotion = fields.Boolean(string='Promotion', default=False)
+    x_brand = fields.Char(string='Marque')
+    x_specs = fields.Text(string='Caractéristiques')
