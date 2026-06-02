bb4a0050d7b401be458941b1af5c11be583c9f20
Chouikhi Imene <imene.chouikhi@etu.univ-lyon1.fr>
2026-06-02 18:58:37 +0200

    fix

--- website_planet_mobil/models/product_extension.py
+++ website_planet_mobil/models/product_extension.py
@@ -7,25 +7,6 @@ class ProductTemplate(models.Model):
 
     x_is_top_vente = fields.Boolean(string='Top Vente', default=False)
     x_is_nouveaute = fields.Boolean(string='Nouveauté', default=False)
-    x_is_promotion = fields.Boolean(
-        string='Promotion',
-        compute='_compute_is_promotion',
-        store=True
-    )
+    x_is_promotion = fields.Boolean(string='Promotion', default=False)
     x_brand = fields.Char(string='Marque')
     x_specs = fields.Text(string='Caractéristiques')
-
-    @api.depends('item_ids', 'list_price')
-    def _compute_is_promotion(self):
-        for product in self:
-            product.x_is_promotion = bool(
-                product.item_ids.filtered(
-                    lambda i: (
-                        i.compute_price == 'discount' and i.price_discount > 0
-                    ) or (
-                        i.compute_price == 'formula' and (i.price_discount > 0 or i.price_extra < 0)
-                    ) or (
-                        i.compute_price == 'fixed' and i.fixed_price < product.list_price
-                    )
-                )
-            )
