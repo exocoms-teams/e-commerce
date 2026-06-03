d27011988370c8fe15b5b335386939075ccbd6d6
Chouikhi Imene <imene.chouikhi@etu.univ-lyon1.fr>
2026-06-03 14:47:06 +0200

    test /promo

--- website_planet_mobil/models/product_extension.py
+++ website_planet_mobil/models/product_extension.py
@@ -7,6 +7,15 @@ class ProductTemplate(models.Model):
 
     x_is_top_vente = fields.Boolean(string='Top Vente', default=False)
     x_is_nouveaute = fields.Boolean(string='Nouveauté', default=False)
-    x_is_promotion = fields.Boolean(string='Promotion', default=False)
+    x_is_promotion = fields.Boolean(
+        string='Promotion',
+        compute='_compute_is_promotion',
+        store=True
+    )  
     x_brand = fields.Char(string='Marque')
     x_specs = fields.Text(string='Caractéristiques')
+
+    @api.depends('pricelist_rule_ids')
+    def _compute_is_promotion(self):
+        for product in self:
+            product.x_is_promotion = bool(product.pricelist_rule_ids)
