8bb920834f00aaeda559b4123a2802940eb3a4a1
Noura250 <g.issa@ecoles-epsi.net>
2026-06-05 13:40:13 +0200

    v4 - hero image fixe

---
+++ monetique_theme/controllers/main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+
+
+class MonetiqueFrontend(http.Controller):
+
+    @http.route('/', type='http', auth='public', website=True, sitemap=True)
+    def home(self, **kw):
+        return request.render('monetique_theme.page_home', {})
