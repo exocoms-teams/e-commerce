ab46a68fd9cc291902a3f8bfe40589216fa8bae3
Lareinne Kouonang <142983088+lveranne@users.noreply.github.com>
2026-06-07 11:24:21 +0200

    Uncomment import for api_assurance module

--- custom_addons/sinistre_services/controllers/__init__.py
+++ custom_addons/sinistre_services/controllers/__init__.py
@@ -3,5 +3,4 @@ from . import api_controller
 from . import website_controller
 from . import api_pwa
 from . import pwa_controller
-# api_assurance chargé séparément — déployer api_assurance.py avant de décommenter
-# from . import api_assurance
+from . import api_assurance
