4faba287f92cb56ace264de337a799994fe22294
Cyrine <cyrineboudjelel@gmail.com>
2026-06-09 20:10:41 +0200

    rs

--- monetique_theme/__manifest__.py
+++ monetique_theme/__manifest__.py
@@ -10,6 +10,7 @@
         'views/templates/layout.xml',
         'views/templates/components.xml',
         'views/pages/home.xml',
+        'views/payment_templates.xml',
     ],
     'assets': {
         'web.assets_frontend': [
