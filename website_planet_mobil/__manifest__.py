6d922d8c99fe6df721cf251ef3b4c6df0ded276a
Laeticia <laeticiabamba@icloud.com>
2026-06-15 16:59:12 +0200

    fix

--- website_planet_mobil/__manifest__.py
+++ website_planet_mobil/__manifest__.py
@@ -26,6 +26,7 @@
         'data/product_detail_views.xml',
         'templates/page.xml',
         'templates/politiques_cookies.xml',
+        'data/cookies_bar.xml',
     ],
     'assets': {
         'web.assets_frontend': [
