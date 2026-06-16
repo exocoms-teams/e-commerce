6627d9e77858d2b0b2b673e14ef06f0d2ff879fd
Stella <stellamekuigne@gmail.com>
2026-06-16 16:51:10 +0200

    fix: update

--- multi_vendor_marketplace/models/sale_order_line.py
+++ multi_vendor_marketplace/models/sale_order_line.py
@@ -13,7 +13,7 @@
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
-#
+# 
 #    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
 #    (LGPL v3) along with this program.
 #    If not, see <http://www.gnu.org/licenses/>.
