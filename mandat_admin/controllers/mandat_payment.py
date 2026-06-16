253afd56b2135ab70de2fd36e29fc94df0f99d23
Matteo <matteo.galet@etu.inp-n7.fr>
2026-06-16 11:38:01 +0200

    fix: supprimer champs dupliqués et type json déprécié

--- mandat_admin/controllers/mandat_payment.py
+++ mandat_admin/controllers/mandat_payment.py
@@ -6,22 +6,21 @@ import json
 
 class MandatPaymentController(http.Controller):
 
-    @http.route('/mandat/save_checkout_data', type='json', auth='public', website=True)
+    @http.route('/mandat/save_checkout_data', type='jsonrpc', auth='public', website=True)
     def save_mandat_checkout_data(self, **kwargs):
         order = request.website.sale_get_order()
         if not order:
             return {'success': False, 'error': 'Commande introuvable'}
 
         order.write({
-            'mandat_checkout_siret': kwargs.get('siret', ''),
-            'mandat_checkout_iban': kwargs.get('iban', ''),
-            'mandat_checkout_ordonnateur': kwargs.get('ordonnateur', ''),
-            'mandat_checkout_qualite': kwargs.get('qualite', ''),
-            'mandat_checkout_comptable': kwargs.get('comptable', ''),
-            'mandat_checkout_ej': kwargs.get('ej', ''),
-            'mandat_checkout_service': kwargs.get('service', ''),
-            'mandat_checkout_reference': kwargs.get('reference', ''),
-            'mandat_checkout_filled': True,
+            'acheteur_siret': kwargs.get('siret', ''),
+            'fournisseur_iban': kwargs.get('iban', ''),
+            'ordonnateur': kwargs.get('ordonnateur', ''),
+            'qualite_ordonnateur': kwargs.get('qualite', ''),
+            'comptable_public': kwargs.get('comptable', ''),
+            'numero_engagement': kwargs.get('ej', ''),
+            'acheteur_service': kwargs.get('service', ''),
+            'reference_bon_commande': kwargs.get('reference', ''),
             'payment_mode': 'mandat_administratif',
         })
         return {'success': True}
