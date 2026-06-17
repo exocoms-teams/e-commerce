67be2f7b431a867682e5e4f2326779148b627ee6
tareguemariejosephe <mariejosephetaregue@gmail.com>
2026-06-17 12:54:36 +0200

    Modfication de la page

--- Matelas/__manifest__.py
+++ Matelas/__manifest__.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Matelas',
-    'version': '1.0',
+    'version': '19.0.1.0.10',
     'summary': 'Site e-commerce de vente de matelas',
     'category': 'Website',
     'depends': ['website', 'website_sale'],
     'data': [
-        'views/Home.xml',
+        'views/templates/Home.xml',
     ],
+    'assets': {
+        'web.assets_frontend': [
+            'Matelas/static/src/css/Home.css',
+        ],
+    },
     'installable': True,
     'application': True,
+    'auto_install': False,
+    'licence': 'LGPL-3',
 }
-
-'assets': {
-    'web.assets_frontend': [
-        'Matelas/static/src/css/.css',
-    ],
-},
