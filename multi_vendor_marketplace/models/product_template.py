885ccd27d8babe02cbe5bc5b1f5788d16ff94c80
Stella <stellamekuigne@gmail.com>
2026-06-17 11:37:59 +0200

    fix: update sale page

--- multi_vendor_marketplace/models/product_template.py
+++ multi_vendor_marketplace/models/product_template.py
@@ -5,7 +5,7 @@
 #
 #    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
 #    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
-#
+# 
 #    You can modify it under the terms of the GNU LESSER
 #    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
 #
