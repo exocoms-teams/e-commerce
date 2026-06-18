25d825b3255645e27e46a9bed1644424536a8e67
MaramHadjAli <maram.hadjali@enicar.ucar.tn>
2026-06-18 21:05:42 +0100

    fixes

--- oa_beauty_theme/__manifest__.py
+++ oa_beauty_theme/__manifest__.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 {
     'name': 'LUMIÈRE Beauty Core',
     'version': '1.0',
     'category': 'Website/E-commerce',
     'summary': 'Gestion personnalisée des fiches cosmétiques pour LUMIÈRE Beauty',
+    'description': """Ce module ajoute des champs personnalisés pour la marque LUMIÈRE :
+- Type de cosmétique
+- Finition (Mat, Prismatic Glow, Gloss...)
+- Ingrédients clés (Beurre de karité, Vitamine E...)
+- Gestion des teintes et des recommandations de peau.""",
     'author': 'Maram Hadj Ali',
     'depends': ['website_sale', 'product'],
     'data': [
         'security/ir.model.access.csv',
         'views/product_template_views.xml',
     ],
-    # Configuration des assets pour charger votre CSS sur le Front-End (Site Web)
     'assets': {
         'web.assets_frontend': [
             '/lumiere_beauty/static/src/css/lumiere_style.css',
