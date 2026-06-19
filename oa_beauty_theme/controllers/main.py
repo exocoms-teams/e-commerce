0c574108bdfe628f264143b7dbebf6732bdd4438
MaramHadjAli <maram.hadjali@enicar.ucar.tn>
2026-06-19 12:54:18 +0100

    fix odoo bug

--- oa_beauty_theme/controllers/main.py
+++ oa_beauty_theme/controllers/main.py
@@ -6,19 +6,9 @@ _logger = logging.getLogger(__name__)
 
 
 class OaBeautyHomepage(http.Controller):
-    """
-    Override the default Odoo homepage route.
-
-    Odoo's website module defines its own '/' handler in
-    odoo/addons/website/controllers/main.py (class Website, method index).
-    By declaring the same route in a dependent module, our handler takes
-    precedence because Odoo picks the last-registered route for a given
-    URL+method combination, and addons load after core modules.
-    """
 
     @http.route('/', type='http', auth='public', website=True)
     def homepage(self, **kw):
-        # Fetch up to 3 published products for the shop preview section.
         try:
             products = request.env['product.template'].sudo().search(
                 [('is_published', '=', True), ('sale_ok', '=', True)],
@@ -61,30 +51,24 @@ class OaBeautyContact(http.Controller):
             to_email = company.email or 'contact@oabeauty.example'
 
             body_html = (
-                '<p><strong>Name:</strong> {name}</p>'
-                '<p><strong>Email:</strong> {email}</p>'
-                '<p><strong>Phone:</strong> {phone}</p>'
-                '<p><strong>Subject:</strong> {subject}</p>'
+                '<p><strong>Name:</strong> ' + from_name + '</p>'
+                '<p><strong>Email:</strong> ' + email + '</p>'
+                '<p><strong>Phone:</strong> ' + (phone or '-') + '</p>'
+                '<p><strong>Subject:</strong> ' + (subject or '-') + '</p>'
                 '<hr/>'
-                '<p>{message}</p>'
-            ).format(
-                name=from_name,
-                email=email,
-                phone=phone or '-',
-                subject=subject or '-',
-                message=message.replace('\n', '<br/>'),
+                '<p>' + message.replace('\n', '<br/>') + '</p>'
             )
 
             mail = request.env['mail.mail'].sudo().create({
-                'subject': '[O&A Beauty] Contact - %s' % (subject or 'Website Enquiry'),
+                'subject': '[O&A Beauty] ' + (subject or 'Website Enquiry'),
                 'body_html': body_html,
-                'email_from': '%s <%s>' % (from_name, email),
+                'email_from': from_name + ' <' + email + '>',
                 'email_to': to_email,
             })
             mail.sudo().send()
 
         except Exception:
-            _logger.exception('O&A Beauty contact form failed to send mail')
+            _logger.exception('OaBeauty contact form error')
             return request.redirect('/?contact_error=1')
 
         return request.redirect('/?contact_sent=1')
