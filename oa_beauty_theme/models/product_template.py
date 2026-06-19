eb6d995dd8bb9b642619e12f34702c82bd80e257
MaramHadjAli <maram.hadjali@enicar.ucar.tn>
2026-06-19 14:54:17 +0100

    fix .py

--- oa_beauty_theme/models/product_template.py
+++ oa_beauty_theme/models/product_template.py
@@ -4,7 +4,7 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    oa_type = fields.Char(string='Product Type')
+    oa_type = fields.Char(string='OA Product Type')
     oa_finish = fields.Char(string='Finish')
     oa_best_for = fields.Char(string='Best For')
     oa_key_ingredients = fields.Char(string='Key Ingredients')
