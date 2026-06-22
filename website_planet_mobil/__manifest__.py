dd765ba4e114c8a1f7ca896c259e51a91125c9ad
Chouikhi Imene <imene.chouikhi@etu.univ-lyon1.fr>
2026-06-22 16:48:15 +0200

    fix

--- website_planet_mobil/__manifest__.py
+++ website_planet_mobil/__manifest__.py
@@ -25,7 +25,6 @@
         'data/homepage_views.xml',
         'data/product_detail_views.xml',
         'data/reviews.xml',
-        'data/menus.xml',
         'templates/page.xml',
         'templates/politiques_cookies.xml',
         
