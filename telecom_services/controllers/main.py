1fa2b2556044b78c189b0ac897bb13d7f209f37e
IliesBossuyt <ilies.bossuyt@ynov.com>
2026-06-26 16:08:07 +0200

    FixLanguage

--- telecom_services/controllers/main.py
+++ telecom_services/controllers/main.py
@@ -1,6 +1,5 @@
 from odoo import http
 from odoo.http import request
-from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
 class TelecomController(http.Controller):
@@ -42,8 +41,6 @@ class TelecomController(http.Controller):
             for categ in subcategories:
                 products = ProductTemplate.search([
                     ('public_categ_ids', 'in', categ.ids),
-                    ('is_published', '=', True),
-                    ('sale_ok', '=', True),
                 ])
                 universes.append({
                     'category': categ,
@@ -57,25 +54,3 @@ class TelecomController(http.Controller):
             'universes': universes,
             **strings,
         })
-
-
-class TelecomShopOverride(WebsiteSale):
-
-    def _get_search_domain(self, search, category, attrib_values, search_in_description=True):
-        domain = super()._get_search_domain(
-            search, category, attrib_values, search_in_description
-        )
-        return domain + self._telecom_exclusion_domain()
-
-    def _telecom_exclusion_domain(self):
-        root_categ = request.env.ref(
-            'telecom_services.categ_telecom', raise_if_not_found=False
-        )
-        if not root_categ:
-            return []
-        telecom_categs = request.env['product.public.category'].sudo().search(
-            [('id', 'child_of', root_categ.id)]
-        )
-        if not telecom_categs:
-            return []
-        return [('public_categ_ids', 'not in', telecom_categs.ids)]
