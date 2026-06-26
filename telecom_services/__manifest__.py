49ba01aeb93bc25eba7bb0586f89563a717b09f0
IliesBossuyt <ilies.bossuyt@ynov.com>
2026-06-26 17:28:17 +0200

    Description

--- telecom_services/__manifest__.py
+++ telecom_services/__manifest__.py
@@ -1,18 +1,18 @@
 {
     'name': 'Telecom Services',
-    'version': '19.0.1.0.0',
+    'version': '19.0.1.1.0',
     'summary': 'Mega-menu Télécom et catalogue de services Dstny',
     'category': 'Website',
     'author': 'Exocoms',
     'website': 'https://exocoms.fr',
     'depends': ['website', 'website_sale'],
     'data': [
-        'data/delete_categories.xml',
         'data/products.xml',
         'data/menu_data.xml',
         'data/translations_fr.xml',
         'data/translations_en.xml',
         'views/telecom_page.xml',
+        'views/product_page.xml',
     ],
     'assets': {
         'web.assets_frontend': [
