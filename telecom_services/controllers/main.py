cf67d5bc4f5979ee30abf2cdfe5dfabd0d277e16
IliesBossuyt <ilies.bossuyt@ynov.com>
2026-06-26 14:24:28 +0200

    FixLanguage

--- telecom_services/controllers/main.py
+++ telecom_services/controllers/main.py
@@ -5,6 +5,25 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 class TelecomController(http.Controller):
 
+    _STRINGS = {
+        'fr_FR': {
+            'page_title': 'Solutions Télécom',
+            'page_subtitle': (
+                'Voix, mobilité, intelligence conversationnelle, '
+                'connectivité et cybersécurité pour les professionnels.'
+            ),
+            'voir_service': 'Voir le service',
+        },
+        'en_US': {
+            'page_title': 'Telecom Solutions',
+            'page_subtitle': (
+                'Voice, mobility, conversational intelligence, '
+                'connectivity and cybersecurity for professionals.'
+            ),
+            'voir_service': 'View service',
+        },
+    }
+
     @http.route('/telecom', type='http', auth='public', website=True)
     def telecom_page(self, **kwargs):
         ProductCateg = request.env['product.public.category'].sudo()
@@ -31,8 +50,12 @@ class TelecomController(http.Controller):
                     'products': products,
                 })
 
+        lang = request.env.context.get('lang', 'fr_FR')
+        strings = self._STRINGS.get(lang, self._STRINGS['fr_FR'])
+
         return request.render('telecom_services.telecom_page', {
             'universes': universes,
+            **strings,
         })
 
 
