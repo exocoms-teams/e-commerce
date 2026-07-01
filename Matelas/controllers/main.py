361d4661f993de0abf99ea0ab177d12ddc30510d
tareguemariejosephe <mariejosephetaregue@gmail.com>
2026-07-01 12:10:10 +0200

    Modifictaion de la page avis et du footer

--- Matelas/controllers/main.py
+++ Matelas/controllers/main.py
@@ -10,15 +10,28 @@ class MatelasVente(http.Controller):
             ('is_published', '=', True)
         ], limit=6)
         return request.render('Matelas.home', {'products': products})
-    
+
     @http.route('/avis', auth='public', website=True)
     def avis(self, **kwargs):
-       return request.render('Matelas.avis_page', {})
-    
+        partner = request.env.user.partner_id
+        a_achete = False
+
+        if request.env.user.id != request.env.ref('base.public_user').id:
+            commandes = request.env['sale.order'].sudo().search([
+                ('partner_id', '=', partner.id),
+                ('state', 'in', ['sale', 'done']),
+            ], limit=1)
+            a_achete = bool(commandes)
+
+        return request.render('Matelas.avis_page', {
+            'a_achete': a_achete,
+            'user_connected': request.env.user.id != request.env.ref('base.public_user').id,
+        })
+
     @http.route('/contact', auth='public', website=True)
     def contact(self, **kwargs):
-       return request.render('Matelas.contact_page', {})
-    
+        return request.render('Matelas.contact_page', {})
+
     @http.route('/mentions-legales', auth='public', website=True)
     def mentions_legales(self, **kwargs):
-       return request.render('Matelas.mentions_legales', {})
+        return request.render('Matelas.mentions_legales', {})
