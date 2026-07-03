cf46456acf7224f26a98258e2d276cfc130a8918
Yassine Tartor <yassinetartor5@gmail.com>
2026-07-03 12:17:05 +0100

    Ajout du module custom_supplements pour le nouveau projet

---
+++ custom_supplements/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Boutique Compléments Alimentaires',
+    'version': '1.0',
+    'summary': 'Vente de protéines, vitamines et gestion des stocks',
+    'category': 'eCommerce',
+    'depends': [
+        'base',
+        'website_sale',
+        'stock',        
+    ],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
