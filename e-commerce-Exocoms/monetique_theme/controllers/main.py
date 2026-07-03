5d9013fb6ed93d941170544640046c661564f6bb
Ahmed BEN NACIB <ahmed.ben-nacib@etu.univ-grenoble-alpes.fr>
2026-07-03 06:26:04 +0200

    fix priorite

--- e-commerce-Exocoms/monetique_theme/controllers/main.py
+++ e-commerce-Exocoms/monetique_theme/controllers/main.py
@@ -1,8 +1,23 @@
 from odoo import http
 from odoo.http import request
 
+
 class Monetique(http.Controller):
 
     @http.route('/', type='http', auth='public', website=True, sitemap=True)
     def home(self, **kw):
+        IrModule = request.env['ir.module.module'].sudo()
+        sidebar_modules = [
+            'exocoms_sidebar_cards',
+            'exocoms_sidebar_tree',
+            'exocoms_sidebar_accordion',
+        ]
+        for module_name in sidebar_modules:
+            module = IrModule.search([
+                ('name', '=', module_name),
+                ('state', '=', 'installed'),
+            ], limit=1)
+            if module:
+                return request.redirect('/boutique')
+
         return request.render('monetique_theme.page_home', {})
