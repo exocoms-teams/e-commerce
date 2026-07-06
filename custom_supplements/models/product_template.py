95333c8338d0b81475f4472cdfd3a9e4bfceaad4
Yassine Tartor <yassinetartor5@gmail.com>
2026-07-06 11:51:33 +0100

    Fix: Utilisation de model_create_multi pour corriger l'erreur d'installation

--- custom_supplements/models/product_template.py
+++ custom_supplements/models/product_template.py
@@ -1,33 +1,26 @@
 from odoo import models, fields, api
 
 class ProductTemplate(models.Model):
-    # L'héritage: 
     _inherit = 'product.template'
 
-    # Ajout des nouveaux champs spécifiques aux compléments alimentaires
     is_vegan = fields.Boolean(string="100% Vegan", default=False)
-    
-    # Tableau des macros (Protéines, Glucides...)
     nutritional_info = fields.Html(string="Valeurs Nutritionnelles")
+    allergens = fields.Char(string="Allergènes")
     
-    # Un champ  pour les allergènes
-    allergens = fields.Char(
-        string="Allergènes", 
-        help="Indiquez ici les allergènes présents (ex: Lait, Soja, Arachides)"
-    )
-
-   # On passe le default à False. C'est au responsable de cocher la case pour tes compléments.
     is_supplement = fields.Boolean(string="Est un complément alimentaire", default=False)
 
-    @api.model
-    def create(self, vals):
-        # On n'applique la logistique que si la case est cochée 
-        # ET que le produit est de type 'product' (Article Stockable), pas un 'service'.
-        if vals.get('is_supplement', False) and vals.get('type', 'consu') == 'product':
-            vals['tracking'] = 'lot'
-            vals['use_expiration_date'] = True
-            
-            if not vals.get('expiration_time'):
-                vals['expiration_time'] = 365
+    # CORRECTION : On utilise le décorateur 'multi' pour gérer les listes
+    @api.model_create_multi
+    def create(self, vals_list):
+        # On fait une boucle pour analyser chaque produit de la liste
+        for vals in vals_list:
+            # Si le produit actuel est marqué comme complément
+            if vals.get('is_supplement', False):
+                vals['tracking'] = 'lot'
+                vals['use_expiration_date'] = True
                 
-        return super(ProductTemplate, self).create(vals)
+                if not vals.get('expiration_time'):
+                    vals['expiration_time'] = 365
+                    
+        # On passe la liste modifiée à la vraie méthode d'Odoo
+        return super(ProductTemplate, self).create(vals_list)
