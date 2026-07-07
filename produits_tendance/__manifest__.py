85b639e35cd63f82a880416a47dc8e53f2c45071
Hicham <mouloudihicham.bac1@gmail.com>
2026-07-07 20:08:47 +0100

    Création du module **Produit_Tendance** avec son architecture interne.

---
+++ produits_tendance/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Produits Tendance',
+    'version': '19.0.1.0.0',
+    'category': 'Website',
+    'author': 'winners',
+    'license': 'LGPL-3',
+    'depends': ['website', 'website_sale'],
+    
+    'installable': True,
+    'application': False,
+}
