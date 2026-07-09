7d14c7745c9fb4f22844afbfa943de0230aaf0f4
Lareinne Kouonang <142983088+lveranne@users.noreply.github.com>
2026-07-09 07:32:30 +0200

    Reorder imports in __init__.py

--- custom_addons/sinistre_services/models/__init__.py
+++ custom_addons/sinistre_services/models/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from . import mission
-from . import document_artisan
 from . import intervenant
-from . import assurance
 from . import devis
+from . import document_artisan
+from . import assurance
 from . import commission
 from . import photo_dossier
 from . import message
