63e59bd330529d9a1bdb8cc7a297c194b1ecbb12
Yassine Tartor <yassinetartor5@gmail.com>
2026-07-20 12:14:18 +0100

    fix: utilisation de osv.expression.AND pour la gestion du domaine vegan

--- custom_supplements/models/website.py
+++ custom_supplements/models/website.py
@@ -1,16 +1,17 @@
 from odoo import models
 from odoo.http import request
+from odoo.osv import expression
 
 class Website(models.Model):
     _inherit = 'website'
 
     def sale_product_domain(self):
-        # 1. On récupère le domaine de base strict (produits publiés, disponibles, etc.)
+        # 1. On récupère le domaine de base (qui est un objet DomainAnd)
         domain = super().sale_product_domain()
         
-        # 2. Vérification sécurisée : sommes-nous dans une requête web contenant "?vegan=1" ?
-        # L'utilisation de hasattr évite les plantages lors des tâches CRON internes
+        # 2. Vérification sécurisée : l'URL contient-elle "?vegan=1" ?
         if request and hasattr(request, 'params') and request.params.get('vegan'):
-            domain.append(('is_vegan', '=', True))
+            # 3. On utilise l'outil Odoo pour fusionner le domaine existant avec notre condition
+            domain = expression.AND([domain, [('is_vegan', '=', True)]])
             
         return domain
