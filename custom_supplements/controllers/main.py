654e907d5fda48075ce8a08fde42fc1bb247077f
Yassine Tartor <yassinetartor5@gmail.com>
2026-07-20 11:13:48 +0100

    feat(controllers): ajout du filtre dynamique vegan sur la page shop

---
+++ custom_supplements/controllers/main.py
@@ -0,0 +1,19 @@
+from odoo import http
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+class WebsiteSaleSupplements(WebsiteSale):
+    
+    # On surcharge la méthode qui prépare la recherche des produits
+    def _get_search_domain(self, search, category, attrib_values, search_in_description=True):
+        # 1. On récupère le domaine de recherche standard d'Odoo
+        domain = super(WebsiteSaleSupplements, self)._get_search_domain(
+            search, category, attrib_values, search_in_description
+        )
+        
+        # 2. Si le paramètre 'vegan' est détecté dans l'URL (ex: /shop?vegan=1)
+        if request.params.get('vegan'):
+            # On ajoute notre propre condition : le champ is_vegan doit être True
+            domain.append(('is_vegan', '=', True))
+        
+        return domain
