f58db7d71a72d62b11e3ef0468c773f35d2efe3a
Salma <salmabenmakia37@gmail.com>
2026-07-21 22:27:33 +0100

    rename monetique_theme to sneakers

--- sneakers/controllers/main.py
+++ sneakers/controllers/main.py
@@ -2,28 +2,28 @@ from odoo import http
 from odoo.http import request
 
 
-class MonetiqueThemeController(http.Controller):
+class SneakersController(http.Controller):
 
 
     @http.route('/', type='http', auth='public', website=True, sitemap=True)
     def home(self, **kwargs):
-        return request.render('monetique_theme.page_home', {})
+        return request.render('sneakers.page_home', {})
 
     @http.route('/shop', type='http', auth='public', website=True, sitemap=True)
     def shop(self, **kwargs):
-        return request.render('monetique_theme.page_shop', {})
+        return request.render('sneakers.page_shop', {})
     @http.route('/product', type='http', auth='public', website=True, sitemap=True)
     def product(self, **kwargs):
-        return request.render('monetique_theme.page_product', {})
+        return request.render('sneakers.page_product', {})
 
     @http.route('/cart', type='http', auth='public', website=True, sitemap=True)
     def cart(self, **kwargs):
-        return request.render('monetique_theme.page_cart', {})
+        return request.render('sneakers.page_cart', {})
 
     @http.route('/checkout', type='http', auth='public', website=True, sitemap=True)
     def checkout(self, **kwargs):
-        return request.render('monetique_theme.page_checkout', {})
+        return request.render('sneakers.page_checkout', {})
 
     @http.route('/confirmation', type='http', auth='public', website=True, sitemap=True)
     def confirmation(self, **kwargs):
-        return request.render('monetique_theme.page_confirmation', {})
+        return request.render('sneakers.page_confirmation', {})
