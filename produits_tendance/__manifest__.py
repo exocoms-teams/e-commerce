738dd358913ee8fe4430af9eae1d80da2a3b83a7
zchalal5 <chalal.zineb@etu.uae.ac.ma>
2026-07-22 14:45:49 +0100

    WIN-47: signup form auth_signup override + groupe utilisateur gratuit

--- produits_tendance/__manifest__.py
+++ produits_tendance/__manifest__.py
@@ -4,14 +4,15 @@
     'category': 'Website',
     'author': 'winners',
     'license': 'LGPL-3',
-    'depends': ['website', 'website_sale'],
+   'depends': ['website', 'website_sale', 'auth_signup'],
 
-    'data': [
-        'security/security_groups.xml',
-        'security/ir.model.access.csv',
-        'views/trend_ad_views.xml',
-        'views/website_templates.xml',
-        'views/trend_product_views.xml',
+   'data': [
+            'security/security_groups.xml',
+            'security/ir.model.access.csv',
+            'views/trend_ad_views.xml',
+            'views/website_templates.xml',
+            'views/trend_product_views.xml',
+            'views/auth_templates.xml',   # nouveau
     ],
 
     'installable': True,
