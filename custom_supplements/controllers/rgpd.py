aa1cecd6f7413df2aebfcc2f5c83f245cfc77b00
Theanael <theanael@gmail.com>
2026-08-12 16:49:14 +0200

    test

--- custom_supplements/controllers/rgpd.py
+++ custom_supplements/controllers/rgpd.py
@@ -60,7 +60,7 @@ class CustomPortal(CustomerPortal):
                     ('model', '=', 'res.partner'),
                     ('res_id', '=', partner_id),
                 ])
-                _logger.warning("CECI EST UN TEST")
+                _logger.warning("CECI EST UN TEST %s", messages.count())
                 for message in messages:
                     _logger.warning(
                         "MESSAGE %s | date=%s | subtype=%s | tracking=%s | body=%s",
