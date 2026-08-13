5c85f797041654186b553cfe68a266280f063e5b
Hicham <mouloudihicham.bac1@gmail.com>
2026-08-13 16:47:23 +0100

    modification de style

--- produits_tendance/__manifest__.py
+++ produits_tendance/__manifest__.py
@@ -45,6 +45,7 @@
             'produits_tendance/static/src/scss/subscription_pricing.scss',
             'produits_tendance/static/src/scss/dashboard.scss',
             'produits_tendance/static/src/scss/header.scss',
+            'produits_tendance/static/src/scss/footer.scss',
             'produits_tendance/static/src/js/dashboard_filters.js',
         ],
     },
