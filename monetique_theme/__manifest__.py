eab8499fe33460003c0df91df4ef7da60923fc18
Salma <salmabenmakia37@gmail.com>
2026-08-16 00:53:00 +0100

    old design

--- monetique_theme/__manifest__.py
+++ monetique_theme/__manifest__.py
@@ -9,7 +9,7 @@
     'data': [
         'security/ir.model.access.csv',
 
-        'views/pages/home.xml',
+        'views/pages/home1.xml',
 
         'views/templates/header.xml',
         'views/templates/footer.xml',
@@ -22,7 +22,7 @@
         'web.assets_frontend': [
             'monetique_theme/static/src/css/templates/header.css',
             'monetique_theme/static/src/css/templates/footer.css',
-            'monetique_theme/static/src/css/pages/home.css',
+            'monetique_theme/static/src/css/pages/home1.css',
         ],
     },
     'installable': True,
