62837aa8f55b3df9187cc9957bbe6ed4ef6b603c
Theanael <theanael@gmail.com>
2026-08-19 11:34:33 +0200

    re-adding tests one by one

--- exocoms_rgpd/tests/__init__.py
+++ exocoms_rgpd/tests/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import test_consent_chain
-# from . import test_multi_company
+from . import test_multi_company
 # from . import test_request_workflow
 # from . import test_retention
 # from . import test_blacklist_sync
