e9b18df4b58105680898469cd07fba6f6447c3e0
Theanael <theanael@gmail.com>
2026-08-24 11:21:44 +0200

    test

--- custom_supplements/models/stock_lot.py
+++ custom_supplements/models/stock_lot.py
@@ -18,11 +18,7 @@ class StockLot(models.Model):
             ('expiration_date', '!=', False),
         ])
 
-        expiring_lots = lots.filtered(
-            lambda lot: lot.expiration_date - timedelta(
-                days=lot.product_id.product_tmpl_id.alert_time
-            ) >= now
-        )
+        expiring_lots = lots.filtered(is_lot_expiring)
         _logger.warning('Lots : %s | Expired Lots : %s',lots,expiring_lots)
         if expiring_lots:
             for lot in expiring_lots:
@@ -32,3 +28,24 @@ class StockLot(models.Model):
                     lot.name,
                     lot.expiration_date,
                 )
+
+def is_lot_expiring(lot):
+    alert_time = lot.product_id.product_tmpl_id.alert_time
+
+    if not alert_time:
+        return False
+
+    alert_date = lot.expiration_date - timedelta(days=alert_time)
+
+    _logger.info(
+        "Lot %s | expiration=%s | délai=%s jours | "
+        "date alerte=%s | maintenant=%s | expire bientôt=%s",
+        lot.name,
+        lot.expiration_date,
+        alert_time,
+        alert_date,
+        now,
+        alert_date <= now,
+    )
+
+    return alert_date <= now
