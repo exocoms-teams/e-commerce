2e5205a767f91f2d3e6c1a64d80209d1676e524b
Hicham <mouloudihicham.bac1@gmail.com>
2026-08-24 16:46:29 +0100

    Suppression des modules non intéressants.

--- oa_beauty_theme/controllers/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import advisor_api
-from . import chatbot_api
-from . import main
-from . import tracking
-from . import portal
-from . import newsletter
--- oa_beauty_theme/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import packlink_api
-from . import ai_provider
