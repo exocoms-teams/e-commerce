dd3ab624eff1f2e907893b8485f6484326e6fe00
Theanael <theanael@gmail.com>
2026-08-25 10:16:51 +0200

    test

--- custom_supplements/hooks.py
+++ custom_supplements/hooks.py
@@ -1,6 +1,10 @@
 from odoo import api, SUPERUSER_ID
+import logging
+
+_logger = logging.getLogger(__name__)
 
 def post_init_hook(cr, registry):
+    _logger.warning("purging demo-data")
     env = api.Environment(cr, SUPERUSER_ID, {})
 
     non_supplements = env["product.template"].search([
