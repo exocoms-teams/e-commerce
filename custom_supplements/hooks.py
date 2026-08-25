8c11440ad097123e53114b3f51004ae13cb00469
Theanael <theanael@gmail.com>
2026-08-25 14:46:43 +0200

    test

--- custom_supplements/hooks.py
+++ custom_supplements/hooks.py
@@ -6,7 +6,7 @@ _logger = logging.getLogger(__name__)
 def demo_purge(env):
     _logger.warning("purging demo-data")
 
-    non_supplements = env["product.template"].search([
+    non_supplements = env["product.product"].search([
         # ("is_supplement", "=", False),
         (True,"=",True)
     ])
