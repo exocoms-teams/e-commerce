59af30f28ab15a16f1aa851f481fbb57475f92d9
HassanMarouch <marouch2019@gmail.com>
2026-08-27 15:44:20 +0200

    Updated auth templates and manifest formatting

--- produits_tendance/__manifest__.py
+++ produits_tendance/__manifest__.py
@@ -5,8 +5,12 @@
     'author': 'winners',
     'license': 'LGPL-3',
     'depends': [
-        'website', 'website_sale', 'auth_signup','sale_subscription', 'website_sale_subscription',
-    ],   # WIN-66
+        'website',
+        'website_sale',
+        'auth_signup',
+        'sale_subscription',
+        'website_sale_subscription',
+    ],
     'data': [
         'security/security_groups.xml',
         'security/ir.model.access.csv',
@@ -16,22 +20,21 @@
         'views/trend_submission_views.xml',
         'views/winners_dashboard.xml',
         'views/trend_submission_templates.xml',
-        'views/auth_templates.xml',   # WIN-47
+        'views/auth_templates.xml',
         'views/trend_product_detail_templates.xml',
-        'data/subscription_plans.xml',           # WIN-66
-        'data/subscription_products.xml',        # WIN-66
-        'views/subscription_templates.xml',      # WIN-66
-        'views/dashboard_templates.xml',   # WIN-48 / WIN-45 / WIN-50
-        'data/webhook_queue_cron.xml',      # WIN-67
+        'data/subscription_plans.xml',
+        'data/subscription_products.xml',
+        'views/subscription_templates.xml',
+        'views/dashboard_templates.xml',
+        'data/webhook_queue_cron.xml',
     ],
-
     'demo': [
-        'demo/dashboard_demo.xml',   # WIN-45 / WIN-50 : produits de test pour /dashboard
+        'demo/dashboard_demo.xml',
     ],
     'assets': {
         'web.assets_frontend': [
             'produits_tendance/static/src/scss/_winners_variables.scss',
-            'produits_tendance/static/src/scss/winners_components.scss',   # WIN-105
+            'produits_tendance/static/src/scss/winners_components.scss',
             'produits_tendance/static/src/scss/how_it_works.scss',
             'produits_tendance/static/src/js/winners_dashboard.js',
             'produits_tendance/static/src/scss/trend_submission_form.scss',
@@ -44,16 +47,13 @@
             'produits_tendance/static/src/js/trend_chart.js',
             'produits_tendance/static/src/scss/subscription_pricing.scss',
             'produits_tendance/static/src/scss/dashboard.scss',
-<<<<<<< HEAD
             'produits_tendance/static/src/scss/auth_pages.scss',   # WIN-121
             'produits_tendance/static/src/js/auth_social_toast.js',   # WIN-121
-=======
             'produits_tendance/static/src/scss/header.scss',
             'produits_tendance/static/src/scss/footer.scss',
             'produits_tendance/static/src/scss/product_card.scss',
             'produits_tendance/static/src/scss/home_page.scss',
             'produits_tendance/static/src/js/dashboard_filters.js',
->>>>>>> c51b3c9b1159b8f81a86c6bfdb98e90045191376
         ],
     },
     'installable': True,
